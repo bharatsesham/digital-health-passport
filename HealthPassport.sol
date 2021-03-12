34b96fcfa416e725d5fd05910e963c57226871bd
bharatsesham <sesham.bharat@gmail.com>
2021-03-11 22:29:43 -0500

    Added additional functions and modifiers

--- HealthPassport.sol
+++ HealthPassport.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract HealthPassport {
     
     address public moderator;
+    mapping(address => uint) public balances;
     
     struct HealthAssetHolder {
         address addr;
@@ -12,14 +13,70 @@ contract HealthPassport {
         uint levelThreeKey;
     }
     
+    mapping (address => HealthAssetHolder) public healthDetails;
+    mapping (address=>uint) subscription; 
+
+    
     struct Requestor {
         address addr;
-        uint MaxAccessLevel;        // the highest level a requestor can request. 
+        uint accessLevel;
     }
+    
+    Requestor requestor;
     
     modifier onlyModerator { 
         require(msg.sender == moderator);
         _;
+    }
+    
+    modifier onlyRequestor {
+        require(msg.sender == requestor);
+        _;
+    }
+    
+    modifier maxAccessLevel(uint level)  {
+      if (requestor.accessLevel >= level) {             // the highest level a requestor can request. 
+         _;
+      }
+    }
+    
+    function setAccessLevel(uint level) public onlyModerator {
+        requestor.accessLevel = level;
+    }
+    
+    function register(uint status) public payable {
+        /* 
+            status: 0 -> default (not account)
+            status: 1 -> healthAssetHolder
+            status: 2 -> requestor
+        */
+        address user = msg.sender;
+        
+        if (status == 1) {
+            subscription[user] = 1; 
+        }
+        
+        if (status == 2) {
+            subscription[user] = 2;
+        }
+     }
+    
+    function unregister (address payable user) onlyModerator public {
+        if (moderator!= msg.sender) {
+            revert();
+        }
+        
+        subscription[user] = 0;
+    }
+    
+    function request(address healthAssetHolder, uint levelHash, uint level) onlyRequestor public { 
+        if (subscription[healthAssetHolder] != 1 || requestor[msg.sender].accessLevel < level) {
+            revert();
+        }
+        // How to verify what level does the hash belong to?
+        // How to 
+
+      
     }
 }
 
