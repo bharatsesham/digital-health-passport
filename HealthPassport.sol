2517171fd24dfde638cc92b5da8514fb71f50c22
bharatsesham <sesham.bharat@gmail.com>
2021-03-29 13:38:47 -0400

    Added basic functions.
    Complete the fuctions: request, enroll, register and unregister.

--- HealthPassport.sol
+++ HealthPassport.sol
@@ -1,82 +1,81 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract HealthPassport {
+contract HealthPassort{
     
-    address public moderator;
-    mapping(address => uint) public balances;
-    
+    address moderator;
+
     struct HealthAssetHolder {
-        address addr;
+        string name;
+        uint loinc;
+        bytes32 result;
         uint levelOneKey;
         uint levelTwoKey;
         uint levelThreeKey;
+        uint balance;
     }
-    
-    mapping (address => HealthAssetHolder) public healthDetails;
-    mapping (address=>uint) subscription; 
-
     
     struct Requestor {
-        address addr;
+        string name;
         uint accessLevel;
+        uint balance;
     }
     
-    Requestor requestor;
+    mapping (address => HealthAssetHolder) healthDetails;
+    mapping (address =>  Requestor) requestorDetails;
+
+        
+    /* 
+        subscription: 0 -> default (not account)
+        subscription: 1 -> healthAssetHolder
+        subscription: 2 -> requestor
+        subscription: 3 -> moderator
+    */
+    mapping (address=>uint) public subscription; 
     
     modifier onlyModerator { 
         require(msg.sender == moderator);
         _;
     }
     
-    modifier onlyRequestor {
-        require(msg.sender == requestor);
+    modifier onlyRequestor { 
+        require(subscription[msg.sender] == 2);
         _;
     }
     
-    modifier maxAccessLevel(uint level)  {
-      if (requestor.accessLevel >= level) {             // the highest level a requestor can request. 
-         _;
-      }
+    constructor () payable  {
+        moderator = msg.sender;
+        subscription[msg.sender] = 3;           // Registered as Moderator
     }
     
-    function setAccessLevel(uint level) public onlyModerator {
-        requestor.accessLevel = level;
+    // User can register their health passport using this function
+    function register (string memory _name) public payable {
+        address assetHolder = msg.sender; 
+        healthDetails[msg.sender].name = _name;
+        require(subscription[assetHolder] == 0, "Already registered");
+        subscription[assetHolder] = 1; 
+        healthDetails[msg.sender].balance = msg.value;
     }
     
-    function register(uint status) public payable {
-        /* 
-            status: 0 -> default (not account)
-            status: 1 -> healthAssetHolder
-            status: 2 -> requestor
-        */
-        address user = msg.sender;
-        
-        if (status == 1) {
-            subscription[user] = 1; 
-        }
-        
-        if (status == 2) {
-            subscription[user] = 2;
-        }
-     }
+    // Only Moderator can enroll the requestor with a certain accessLevel permission
+    function enroll (address requestor, uint _accessLevel, string memory _name) public onlyModerator payable {
+        require(subscription[requestor] == 0, "Already registered");
+        subscription[requestor] = 2;
+        requestorDetails[requestor].name = _name;
+        requestorDetails[requestor].accessLevel = _accessLevel;
+        requestorDetails[requestor].balance = msg.value;
+    }
     
-    function unregister (address payable user) onlyModerator public {
-        if (moderator!= msg.sender) {
-            revert();
-        }
-        
+    // Only Moderator can unregister. 
+    function unregister (address user) onlyModerator public {
         subscription[user] = 0;
     }
     
+    // Requestor can use this function to request health details of the asset holder. 
     function request(address healthAssetHolder, uint levelHash, uint level) onlyRequestor public { 
-        if (subscription[healthAssetHolder] != 1 || requestor[msg.sender].accessLevel < level) {
+        if (subscription[healthAssetHolder] != 1 || requestorDetails[msg.sender].accessLevel < level) {
             revert();
         }
-        // How to verify what level does the hash belong to?
-        // How to 
-
-      
+        // Share the details. 
     }
 }
-
